b8dbd5791173a2178fab748b5b57a6381c3c67be
eldanielhumberto <gamekik92@gmail.com>
2025-10-11 15:29:03 -0600

    refactor: adding a comment to avoid confusion with the completedAt variable

--- src/interfaces/Habit.ts
+++ src/interfaces/Habit.ts
@@ -1,5 +1,8 @@
 export interface Habit {
 	id: string;
 	name: string;
+
+	// Save the date when I completed the habit.
+	// If it is null, it means that the habit was not completed 
 	completedAt: Date | null;
 }
