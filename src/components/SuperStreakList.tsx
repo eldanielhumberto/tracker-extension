00a615a2cd0528dbcd1f197b67fbe91516ec205a
eldanielhumberto <gamekik92@gmail.com>
2025-10-14 18:51:38 -0600

    feat: draw days with array

--- src/components/SuperStreakList.tsx
+++ src/components/SuperStreakList.tsx
@@ -1,34 +1,16 @@
 function SuperStreakList() {
+	const days = ['D', 'L', 'M', 'M', 'J', 'V', 'S'];
+
 	return (
 		<div className='flex items-center justify-center gap-6'>
-			<div className='flex flex-col items-center'>
-				<h1 className='text-2xl font-bold'>L</h1>
-				<div className='w-8 h-8 border-4 border-[#232946] rounded-full'></div>
-			</div>
-			<div className='flex flex-col items-center'>
-				<h1 className='text-2xl font-bold'>M</h1>
-				<div className='w-8 h-8 border-4 border-[#232946] rounded-full'></div>
-			</div>
-			<div className='flex flex-col items-center'>
-				<h1 className='text-2xl font-bold'>M</h1>
-				<div className='w-8 h-8 border-4 border-[#232946] rounded-full'></div>
-			</div>
-			<div className='flex flex-col items-center'>
-				<h1 className='text-2xl font-bold'>J</h1>
-				<div className='w-8 h-8 border-4 border-[#232946] rounded-full'></div>
-			</div>
-			<div className='flex flex-col items-center'>
-				<h1 className='text-2xl font-bold'>V</h1>
-				<div className='w-8 h-8 border-4 border-[#232946] rounded-full'></div>
-			</div>
-			<div className='flex flex-col items-center'>
-				<h1 className='text-2xl font-bold'>S</h1>
-				<div className='w-8 h-8 border-4 border-[#232946] rounded-full'></div>
-			</div>
-			<div className='flex flex-col items-center'>
-				<h1 className='text-2xl font-bold'>D</h1>
-				<div className='w-8 h-8 border-4 border-[#232946] rounded-full'></div>
-			</div>
+			{days.map((v, i) => (
+				<div className='flex flex-col items-center'>
+					<h1 className='text-2xl font-bold'>
+						{v} {new Date().getDay() === i ? '*' : null}
+					</h1>
+					<div className='w-8 h-8 border-4 border-[#232946] rounded-full'></div>
+				</div>
+			))}
 		</div>
 	);
 }
