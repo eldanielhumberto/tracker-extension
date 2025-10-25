564e7d5281507a3dc6e9a57cbd9058416c77fae4
eldanielhumberto <gamekik92@gmail.com>
2025-10-24 18:24:41 -0600

    feat: show the streak

--- src/components/SuperStreakList.tsx
+++ src/components/SuperStreakList.tsx
@@ -4,17 +4,21 @@ import { useHabits } from '../hooks/useHabits';
 
 function SuperStreakList() {
 	const { superStreaks } = useHabits();
+	const isStreak = (date: Date) =>
+		superStreaks.find((i) => i === formatDate(date));
 
 	return (
 		<div className='flex items-center justify-center gap-6'>
 			{getCurrentWeek().map((d) => (
 				<div className='flex flex-col items-center'>
-					<h1 className='text-2xl font-bold'>
-						{d.dayName}{' '}
-						{superStreaks.find((i) => i === formatDate(d.date)) &&
-							'*'}
-					</h1>
-					<div className='w-8 h-8 border-4 border-[#232946] rounded-full'></div>
+					<h1 className='text-2xl font-bold'>{d.dayName}</h1>
+					<div
+						className={
+							isStreak(d.date)
+								? 'w-8 h-8 border-4 border-[#232946] rounded-full bg-red-500'
+								: 'w-8 h-8 border-4 border-[#232946] rounded-full'
+						}
+					></div>
 				</div>
 			))}
 		</div>
